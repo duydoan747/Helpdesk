c270552815de22e2d8710cfbce8c46aa32788bd7
duydoan747 <duydoan747@gmail.com>
2025-09-08 09:52:52 +0700

    Update app.py with role-based permissions

--- app.py
+++ app.py
@@ -264,3 +264,4 @@ try:
         )
 except Exception as e:
     st.error(f"❌ Đã gặp lỗi khi tải dữ liệu: {e}")
+tôi đang đưa code này lên Streamlit, tuy nhiên vẫn chưa xác thực được account gmail để nhập thông tin
